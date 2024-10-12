7059aeb3a18e9328880b874e478c7ca29a751768
parkdaiho <158255448+parkdaiho@users.noreply.github.com>
2024-10-12 17:46:20 +0900

    READMD.md updataed

---
+++ src/main/java/me/parkdaiho/developintproject/DevelopintProjectApplication.java
@@ -0,0 +1,13 @@
+package me.parkdaiho.developintproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopintProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopintProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/parkdaiho/developintproject/DevelopintProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.parkdaiho.developintproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopintProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
